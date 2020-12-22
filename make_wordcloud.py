155c786b3c5f4a8891682f52e080ce835dfcb54a
ToLoad <kublaihkan@gmail.com>
2020-12-22 19:38:45 +0900

    fix count

--- make_wordcloud.py
+++ make_wordcloud.py
@@ -18,16 +18,16 @@ def get_freq_used_words(id, text_file, mode):
             nouns.pop(i)
 
     count = Counter(nouns)
-    nouns_list = count.most_common()
+    nouns_list = count.most_common(150)
 
     return nouns_list
 
 def make_png(id, text_file, mode):
     wc = WordCloud(font_path='./font/NanumGothic.ttf',
         background_color='white',
-        max_words=100,
-        width=1000,
-        height=1000)
+        max_words=150,
+        width=500,
+        height=500)
 
     nouns_list = get_freq_used_words(id, text_file, mode)
     wc.generate_from_frequencies(dict(nouns_list))
